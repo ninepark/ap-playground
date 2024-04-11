2981ff7496f5173240f8407c021c34e8f84af205
ninepark <hoophooop111@gmail.com>
2024-04-12 06:57:14 +0900

    wip

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import { useDraggable } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
 
-const Character = ({ src, alt, left, top, onClick: _onClick }) => {
+const Character = ({ id, src, alt, left, top, onClick: _onClick, ...props }) => {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id,
+  });
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
   return (
     <img
       className={'tp-character'}
       src={src}
       alt={alt}
-      style={{ left, top }}
+      style={{ ...style, left, top }}
       onClick={e => {
         e.stopPropagation();
         e.preventDefault();
         _onClick?.(e);
       }}
+      onTouchStartCapture={e => {
+        console.log('onTouchStartCapture', e);
+        props.onTouchStartCapture(e);
+      }}
+      onTouchEndCapture={e => {
+        console.log('onTouchEndCapture', e);
+        props.onTouchEndCapture(e);
+      }}
+      onTouchMoveCapture={e => {
+        console.log('onTouchMoveCapture', e);
+        props.onTouchMoveCapture(e);
+      }}
+      {...props}
     />
   );
 };
--- src/pages/TakePhotos.jsx
+++ src/pages/TakePhotos.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
+import { DndContext, useDndContext, useDraggable, useDroppable, DragOverlay } from '@dnd-kit/core';
+import { CSS } from '@dnd-kit/utilities';
 
 import dolpoong from '../assets/dolpoong.PNG';
 import ggumi from '../assets/ggumi2.png';
@@ -11,6 +13,12 @@ import bajirac from '../assets/bajirac.png';
 import Character from '../components/Character';
 
 const TakePhotos = () => {
+  const videoEl = useRef();
+  const containerEl = useRef();
+  const dndContext = useDndContext();
+  // console.log('dndContext', dndContext);
+
+  const [currentCharacterIdx, setCurrentCharacterIdx] = useState(0);
   const characters = [
     // { name: '돌풍', src: dolpoong },
     { name: '꾸미', src: ggumi },
@@ -21,8 +29,8 @@ const TakePhotos = () => {
     { name: '바지락', src: bajirac },
   ];
   const [displayedCharacters, setDisplayedCharacters] = useState([]);
-  const [currentCharacterIdx, setCurrentCharacterIdx] = useState(0);
-  const videoEl = useRef();
+  const [selectedDisplayedCharacterIdx, setSelectedDisplayedCharacterIdx] = useState(-1);
+
   // Init camera
   const camInit = stream => {
     const cameraView = document.getElementById('cameraview');
@@ -52,17 +60,44 @@ const TakePhotos = () => {
       return;
     }
 
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(camInit)
-      .catch(error => {
-        console.log('get camera permission failed : ', error);
-      });
+    // navigator.mediaDevices
+    //   .getUserMedia({ video: true })
+    //   .then(camInit)
+    //   .catch(error => {
+    //     console.log('get camera permission failed : ', error);
+    //   });
   };
 
   useEffect(() => {
     mainInit();
   }, []);
+
+  const [lastPressed, setLastPressed] = useState(0);
+  const handlePress = useCallback(
+    (event, idx) => {
+      const time = new Date().getTime();
+      const delta = time - lastPressed;
+      setLastPressed(time);
+      if (lastPressed) {
+        if (delta < 200) {
+          console.log('double press');
+          const nextIdx = (currentCharacterIdx + 1) % characters.length;
+          setDisplayedCharacters(prev => {
+            prev.splice(idx, 1, { ...prev[idx], characterIdx: nextIdx });
+            return prev;
+          });
+          setCurrentCharacterIdx(nextIdx);
+        } else {
+          console.log('single press');
+        }
+      }
+    },
+    [lastPressed, characters.length, currentCharacterIdx],
+  );
+
+  const { setNodeRef } = useDroppable({
+    id: 'tp-character-container',
+  });
 
   return (
     <div className={'tp-app'}>
@@ -70,6 +105,7 @@ const TakePhotos = () => {
         <div className={'tp-video-container'}>
           <video id='cameraview' ref={videoEl}></video>
           <div
+            ref={setNodeRef}
             className={'tp-character-container'}
             onClick={e => {
               e.stopPropagation();
@@ -83,25 +119,45 @@ const TakePhotos = () => {
               });
             }}
           >
+            {/*<DndContext*/}
+            {/*// onDragStart={e => {*/}
+            {/*//   console.log('onDragStart', e);*/}
+            {/*// }}*/}
+            {/*// onDragEnd={e => {*/}
+            {/*//   console.log('onDragEnd', e, e.delta.x, e.delta.y);*/}
+            {/*// }}*/}
+            {/*>*/}
             {displayedCharacters.map((character, idx) => {
               return (
                 <Character
                   key={`character-${idx}`}
+                  id={`${characters[character.characterIdx]?.name}-${idx}`}
                   alt={characters[character.characterIdx]?.name}
                   src={characters[character.characterIdx]?.src}
                   left={character.x}
                   top={character.y}
                   onClick={e => {
-                    const nextIdx = (currentCharacterIdx + 1) % characters.length;
-                    setDisplayedCharacters(prev => {
-                      prev.splice(idx, 1, { ...prev[idx], characterIdx: nextIdx });
-                      return prev;
-                    });
-                    setCurrentCharacterIdx(nextIdx);
+                    e.stopPropagation();
+                    e.preventDefault();
+                    handlePress(e, idx);
+                  }}
+                  onTouchStartCapture={e => {
+                    setSelectedDisplayedCharacterIdx(idx);
+                  }}
+                  onTouchMoveCapture={e => {}}
+                  onTouchEndCapture={e => {
+                    setSelectedDisplayedCharacterIdx(-1);
                   }}
                 />
               );
             })}
+            {selectedDisplayedCharacterIdx > -1 ? (
+              <div style={{ width: '10px', height: '10px', backgroundColor: 'red' }}></div>
+            ) : (
+              <></>
+            )}
+            {/*  <DragOverlay>{selectedDisplayedCharacterIdx > -1 ? <></> : null}</DragOverlay>*/}
+            {/*</DndContext>*/}
           </div>
         </div>
       </div>
